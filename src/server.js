b237418734ecc208627fc0eaa2b0745ded2af97b
Johannes Maria Frank <jmfrank63@gmail.com>
2024-05-05 02:47:29 +0700

    Reduce size

--- src/server.js
+++ src/server.js
@@ -21,5 +21,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
